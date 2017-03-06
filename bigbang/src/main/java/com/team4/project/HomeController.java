e37cd586129f45e3e0c0b84d0fe7e565d479d3f2
loverman85 <loverman85@hanmail.net>
2017-03-06 10:20:41 +0900

    Scheduler, Context param 사용 test완료

--- bigbang/src/main/java/com/team4/project/HomeController.java
+++ bigbang/src/main/java/com/team4/project/HomeController.java
@@ -1,39 +1,32 @@
 package com.team4.project;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.ServletContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.ServletContextAware;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController {
+public class HomeController implements ServletContextAware{
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	private ServletContext context;
+
+	@Override
+	public void setServletContext(ServletContext context) {
+		this.context = context;
+	}
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+	public String home() {
+		String admin = context.getInitParameter("test");
+		logger.debug("context.test:"+admin);
+		logger.debug("context.test1:"+context.getInitParameter("test1"));
 		
 		return "home";
 	}
+
 	
 }
