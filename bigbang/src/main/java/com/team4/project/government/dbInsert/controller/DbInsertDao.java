fe060e927692b4f6a64426afcccfa328859094ca
ryun215 <ryun215@naver.com>
2017-03-07 17:39:07 +0900

    수정중

--- bigbang/src/main/java/com/team4/project/government/dbInsert/controller/DbInsertDao.java
+++ bigbang/src/main/java/com/team4/project/government/dbInsert/controller/DbInsertDao.java
@@ -24,5 +24,6 @@ public class DbInsertDao {
 		return 0;
 	}
 	
+	//
 	
 }
--- bigbang/src/main/java/com/team4/project/util/AddHospitalCode.java
+++ bigbang/src/main/java/com/team4/project/util/AddHospitalCode.java
@@ -1,5 +1,14 @@
 package com.team4.project.util;
 
-public class AddHospitalCode {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class AddHospitalCode {
+	private static final Logger logger = LoggerFactory.getLogger(AddHospitalCode.class);
+	public static final String addHospitalCode(String primary){
+		
+		logger.debug("primary 확인 : "+primary);
+		return primary;
+		
+	}
 }
