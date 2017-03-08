2c3ac89bddf0c7f17c0142e877d999bc14151311
KYJ <redkar777@naver.com>
2017-03-08 09:49:37 +0900

    정부백신 DTO

--- bigbang/src/main/java/com/team4/project/government/dto/GoVaccination.java
+++ bigbang/src/main/java/com/team4/project/government/dto/GoVaccination.java
@@ -1,5 +1,32 @@
 package com.team4.project.government.dto;
 
 public class GoVaccination {
-
+	protected String goVaccinationCode;
+	protected String goVaccinationName;
+	protected int goVaccinationDegree;
+	public String getGoVaccinationCode() {
+		return goVaccinationCode;
+	}
+	public void setGoVaccinationCode(String goVaccinationCode) {
+		this.goVaccinationCode = goVaccinationCode;
+	}
+	public String getGoVaccinationName() {
+		return goVaccinationName;
+	}
+	public void setGoVaccinationName(String goVaccinationName) {
+		this.goVaccinationName = goVaccinationName;
+	}
+	public int getGoVaccinationDegree() {
+		return goVaccinationDegree;
+	}
+	public void setGoVaccinationDegree(int goVaccinationDegree) {
+		this.goVaccinationDegree = goVaccinationDegree;
+	}
+	@Override
+	public String toString() {
+		return "GoVaccination [goVaccinationCode=" + goVaccinationCode + ", goVaccinationName=" + goVaccinationName
+				+ ", goVaccinationDegree=" + goVaccinationDegree + "]";
+	}
+	
+	
 }
